19623d6e6a6d3989796d4a319190918ea8aaa4c0
Sarah Prando <sarahprando40@gmail.com>
2023-12-27 18:59:12 -0300

    update vidas js

--- game-mata-mosquito/assents/js/game.js
+++ game-mata-mosquito/assents/js/game.js
@@ -1,3 +1,4 @@
+var vidas = 1;
 var altura = 0;
 var largura = 0;
 
@@ -11,6 +12,19 @@ function ajustarTamanho() {
 ajustarTamanho()
 
 function positionRandom() {
+
+    if (document.getElementById("mosquito")) {
+        document.getElementById("mosquito").remove()
+
+        if (vidas > 3) {
+            alert('Game Over')
+        } else {
+            document.getElementById('v' + vidas).src = "./assents/img/coracao_vazio.png"
+
+            vidas++
+        }
+    }
+
     var positionX = Math.floor(Math.random() * largura) - 90
     var positionY = Math.floor(Math.random() * altura) - 90
 
@@ -21,14 +35,41 @@ function positionRandom() {
 
     var mosquito = document.createElement('img')
     mosquito.src = "./assents/img/mosquito.png"
-    mosquito.className = "mosquito1"
+    mosquito.className = tamanhoAleatorio() + " " + ladoAleatorio()
     mosquito.style.left = positionX + "px"
     mosquito.style.top = positionY + "px"
     mosquito.style.position = "absolute"
+    mosquito.id = "mosquito"
+    mosquito.onclick = function () {
+        this.remove()
+    }
 
     document.body.appendChild(mosquito)
 }
 
 function tamanhoAleatorio() {
     var classe = Math.floor(Math.random() * 3)
+
+    switch (classe) {
+        case 0:
+            return "mosquito1"
+
+        case 1:
+            return "mosquito2"
+
+        case 2:
+            return "mosquito3"
+    }
+}
+
+function ladoAleatorio() {
+    var classe = Math.floor(Math.random() * 2)
+
+    switch (classe) {
+        case 0:
+            return "ladoA"
+
+        case 1:
+            return "ladoB"
+    }
 }
